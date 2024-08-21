d669cec10046309c56edb5cb11a41f46e2ceca58
Angel <santoyox714@gmail.com>
2024-08-20 20:41:08 -0700

    test

--- src/routes/(site)/calendar/+page.svelte
+++ src/routes/(site)/calendar/+page.svelte
@@ -29,3 +29,5 @@
   <div class="iframe-container">
     <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&bgcolor=%23fffcd6&showPrint=0&showTabs=0&showCalendars=0&showTz=0&src=ODY0YTE2YzgyNWE5ODM3ZTEyNmFkZTU5NWM5M2ZlZDQ5OWJmYzY3NDlmMjhlMWZlODI5NmU2YTU5YmZkMDI0MkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23795548" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>  
     </div>
+
+    test
