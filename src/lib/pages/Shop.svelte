b9df118be03505ae4693cc25b7120117373ac1b9
Angel <santoyox714@gmail.com>
2025-02-10 17:34:11 -0800

    Update Shop.svelte

--- src/lib/pages/Shop.svelte
+++ src/lib/pages/Shop.svelte
@@ -3,17 +3,26 @@
   import { shop_cards } from "$lib/utils/shop.ts";
   import ShopCard from "$lib/components/ShopCard.svelte";
   export let backgroundClass = 'bg-white';
-</script>
+  let img_therapy = 'img_therapy.png';</script>
 
 <Page id="Shop" {backgroundClass}>
-  <div class="text-center mb-8">
-    <h1 class="text-3xl m-6 font-light text-red-600">More items coming soon</h1>
-  </div>
+  
 
-  <!-- Grid layout for the cards -->
+  <img src={img_therapy} alt="Centered Image" width = 600
+  style="
+    display: block; 
+    margin: 0 auto; 
+    max-width: 100%; 
+    height: auto;
+  ">
+
   <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
     {#each shop_cards as item}
       <ShopCard {item} />
     {/each}
+  </div>
+
+  <div class="text-center mb-8">
+    <h1 class="text-3xl m-6 font-light text-red-600">More items coming soon</h1>
   </div>
 </Page>
