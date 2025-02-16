0169334a658bbdfc837c0abfa5f609bc790ac940
Angel <santoyox714@gmail.com>
2025-02-15 22:25:35 -0800

    Update shop.ts

--- src/lib/utils/shop.ts
+++ src/lib/utils/shop.ts
@@ -5,7 +5,7 @@ export const shop_cards = [
         img1: "shop/product_1/01.png",
         img2: "shop/product_1/02.png",
         img3: "shop/product_1/03.png",
-        button_id:"buy_btn_1QpL8NCMuPKEpglmL9av9m5W"
+        button_id:"buy_btn_1Qt1GjCMuPKEpglmWQbogUuM"
     },
     {
         id: 2,
